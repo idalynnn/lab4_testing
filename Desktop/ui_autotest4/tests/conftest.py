cb766ca46668fa1ffc67c8a1cc64dc8d886194c4
idalynnn <eliz.telnova@gmail.com>
2025-10-05 06:29:40 +0300

    init2

--- Desktop/ui_autotest4/tests/conftest.py
+++ Desktop/ui_autotest4/tests/conftest.py
@@ -1,4 +1,3 @@
-# добавляем корень проекта в PYTHONPATH
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
@@ -8,14 +7,9 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
-@pytest.fixture(scope="session")   # один браузер на всю сессию
+@pytest.fixture(scope="session")
 def driver():
     opts = Options()
-    # если нужно без окна: раскомментируй
-    # opts.add_argument("--headless")
-
-    # если Firefox установлен нестандартно, пропиши путь:
-    # opts.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 
     d = webdriver.Firefox(service=Service(GeckoDriverManager().install()),
                           options=opts)
